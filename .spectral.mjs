0da44b79e1420bb0df1a16f580ca1caa695199e7
Matthias Friedewald <matthias.friedewald@shopgate.com>
2025-01-27 17:16:23 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3MDg2MA/spectral.js?branch_slug=main&token=36c9fa41-d0bb-4bda-9691-253314212fcf";
+export default { extends: ruleset };
